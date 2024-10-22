3ed089c574952dea8b3a645e737ac2b694c31f91
- manas <rexboy3825@gmail.com>
2024-10-22 21:30:09 +0530

    Formatted the code only

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -20,7 +20,7 @@ public class PlayerController : MonoBehaviour
         playerRB = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
         playerAudio = GetComponent<AudioSource>();
-        
+
         Physics.gravity *= gravityModifier;
 
     }
