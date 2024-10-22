72b76a742595778ab202c08e1b8dbc3e7590050a
- manas <rexboy3825@gmail.com>
2024-10-22 21:28:12 +0530

    Completed the Project
    Completed the basic things in this project

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRB;
-
+    private Animator playerAnim;
+    private AudioSource playerAudio;
+    public ParticleSystem particleExplosion;
+    public ParticleSystem particleSplatter;
+    public AudioClip jumpSfx;
+    public AudioClip crashSfx;
     public float jumpForce = 10f;
     public float gravityModifier;
     public bool isOnGround = true;
@@ -13,16 +18,22 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerRB = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+        
         Physics.gravity *= gravityModifier;
 
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
+            particleSplatter.Stop();
+            playerAudio.PlayOneShot(jumpSfx);
         }
     }
 
@@ -30,12 +41,18 @@ public class PlayerController : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Ground"))
         {
+            particleSplatter.Play();
             isOnGround = true;
         }
         else if (other.gameObject.CompareTag("Obstacle"))
         {
             gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+            particleExplosion.Play();
+            particleSplatter.Stop();
             Debug.Log("Game Over!! ");
+            playerAudio.PlayOneShot(crashSfx);
         }
     }
 }
